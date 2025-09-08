4c8dea1c8b624c958f104482ca18bbf9f9d13f73
elyz <elyzastudy@gmail.com>
2025-09-07 22:22:45 -0300

    Refatora inicialização de structs Area e padroniza formatação em Areas.h e definirAreaNascimento para retorno direto e atualiza grupos de classes.

--- Areas.h
+++ Areas.h
@@ -20,47 +20,44 @@ struct Area{
   char descricao[100]; 
 };
  
-struct Area area1 = {"Castle Town", {"Gárgula", "Assas de Gárgula"},"Descrição de Castle Town"}; 
+struct Area area1 = {"Castle Town", {"Gárgula", "Assas de Gárgula"},"Descrição de Castle Town"};
 struct Area area2 = {"Forest", {"Dragão", "Escama de Dragão"},"Descrição de Florest"};
-struct Area area3 = {"Aetheria", {"Manticore", "Veneno"},"Descrição de Aetheria"}; 
-struct Area area4 = {"Aztlan", {"Gigante de pedra", "Cristais"},"Descrição de Aztlan"}; 
-struct Area area5 = {"Tenebris", {"Peixe", "Escama"},"Descrição de Tenebris"}; 
+struct Area area3 = {"Aetheria", {"Manticore", "Veneno"},"Descrição de Aetheria"};
+struct Area area4 = {"Aztlan", {"Gigante de pedra", "Cristais"},"Descrição de Aztlan"};
+struct Area area5 = {"Tenebris", {"Peixe", "Escama"},"Descrição de Tenebris"};
 
 struct Area* definirAreaNascimento(const char* classedef){   
-  struct Area* suaArea; 
 
-    if (strcmp(classedef, "Guerreiro") == 0 || strcmp(classedef, "Arqueiro") == 0 ||
-      strcmp(classedef, "Ranger") == 0 || strcmp(classedef, "Paladino") == 0 ||
-      strcmp(classedef, "Atirador") == 0 || strcmp(classedef, "cavaleiro") == 0) {
-      suaArea = &area1;
-    }  
-    else if (strcmp(classedef, "Assassino") == 0 || strcmp(classedef, "Bárbaro") == 0 ||
-      strcmp(classedef, "Ladino") == 0) {
-      suaArea = &area4;
-    }  
-    else if (strcmp(classedef, "Bruxo") == 0 || strcmp(classedef, "mago") == 0) {
-      suaArea = &area3;
-    }  
-    else if (strcmp(classedef, "Necromante") == 0) {
-      suaArea = &area5;
-    }  
-    else if (strcmp(classedef, "Druida") == 0) {
-      suaArea = &area2;
-    }   
-    if (suaArea == NULL) {
-      printf("Erro: Classe não reconhecida.\n");
-      return NULL;
-    } 
+  if (strcmp(classedef, "Guerreiro") == 0 || strcmp(classedef, "Arqueiro") == 0 || strcmp(classedef, "Paladino") == 0 ||
+    strcmp(classedef, "Atirador") == 0 || strcmp(classedef, "Cavaleiro") == 0 || strcmp(classedef, "Clerigo") == 0) {
+    return &area1;
+  }  
+  else if (strcmp(classedef, "Assassino") == 0 || strcmp(classedef, "Bárbaro") == 0 ||
+    strcmp(classedef, "Ladino") == 0) {
+    return &area4;
+  }  
+  else if (strcmp(classedef, "Bruxo") == 0 || strcmp(classedef, "mago") == 0) {
+    return &area3;
+  }  
+  else if (strcmp(classedef, "Necromante") == 0) {
+    return &area5;
+  }  
+  else if (strcmp(classedef, "Druida") == 0 || strcmp(classedef, "Ranger") == 0) {
+    return &area2;
+  }   
+  else {
+    printf("Erro: Classe não reconhecida.\n");
+    return NULL;
+  }  
 
   FILE *arquivo = fopen("Perfil.txt", "a+");  
   if (arquivo == NULL) {
     printf("Erro ao abrir o arquivo.\n");
     return NULL; // Ou outra ação apropriada em caso de erro
-  } 
-  printf("\n\nAtenção!\n\tSeu destino está entrelaçado com o local de onde você vem. Logo você nasceu e foi criado(a) em %s. As terras de %s moldaram você, concedendo-lhe as habilidades e conhecimentos únicos que definem sua classe.\n\n", suaArea->nome, suaArea->nome); 
+  }  
+   
+  printf("\n\nO seu destino está entrelaçado com o local de onde você vem. Logo você nasceu e foi criado(a) em %s. As terras de %s moldaram você, concedendo-lhe as habilidades e conhecimentos únicos que definem sua classe.\n\n", suaArea->nome, suaArea->nome); 
   fprintf(arquivo, "\nAtenção!\nSeu destino está entrelaçado com o local de onde você vem. Logo, você nasceu e foi criado(a) em %s. As terras de %s moldaram você, concedendo-lhe as habilidades e conhecimentos únicos que definem sua classe.\n\n", suaArea->nome, suaArea->nome); 
   fclose(arquivo);  
-  
-  return suaArea;
 }
  #endif 
